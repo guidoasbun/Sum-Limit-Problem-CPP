f36e5fded46559126b7b5363b16e5c8ff6bf82d6
Guido Asbun <gdrodrigo8@gmail.com>
2021-02-23 22:30:47 -0800

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Sum_Limit_Problem_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sum_Limit_Problem_CPP.dir/main.cpp.obj"
+  "Sum_Limit_Problem_CPP.exe"
+  "Sum_Limit_Problem_CPP.exe.manifest"
+  "Sum_Limit_Problem_CPP.pdb"
+  "libSum_Limit_Problem_CPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sum_Limit_Problem_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Sum_Limit_Problem_CPP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sum_Limit_Problem_CPP.
+# This may be replaced when dependencies are built.
